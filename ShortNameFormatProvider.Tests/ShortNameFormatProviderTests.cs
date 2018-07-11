db5a03ee7fe4bcca6dd2b104bba7beb495683815
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-12 00:56:04 +0300

    Tests for custom format were created.

---
+++ ShortNameFormatProvider.Tests/ShortNameFormatProviderTests.cs
@@ -0,0 +1,35 @@
+﻿namespace ShortNameFormatProvider.Tests
+{
+    using System;
+
+    using Customer;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Class for testing ShortNameFormatProvider class.
+    /// </summary>
+    [TestFixture]
+    public class ShortNameFormatProviderTests
+    {
+        [TestCase("John Doe", "SP", ExpectedResult = "J. Doe, +1 (425) 555-0100")]
+        [TestCase("John Doe", "SPR", ExpectedResult = "J. Doe, +1 (425) 555-0100, 1000.52")]
+        [TestCase("John Doe", "RS", ExpectedResult = "1000.52, J. Doe")]
+        [TestCase("John Clark Robin Doe", "S", ExpectedResult = "J. C. R. Doe")]
+        public string CustomersToString_ValidInputInvariantCulture_ValidString(string name, string format)
+        {
+            var c = new Customer(name, "+1 (425) 555-0100", 1000.52m);
+            var customFormatProvider = new ShortNameFormatProvider();
+            return string.Format(customFormatProvider, $"{{0:{format}}}", c);
+        }
+
+        [TestCase("SS")]
+        [TestCase("NS")]
+        [TestCase("NSR")]
+        public void CustomersToString_NSInFormat_ThrowsFormatException(string format)
+        {
+            var c = new Customer("John Doe", "+1 (425) 555-0100", 1000.52m);
+            var customFormatProvider = new ShortNameFormatProvider();
+            Assert.Throws<FormatException>(() => string.Format(customFormatProvider, $"{{0:{format}}}", c));
+        }
+    }
+}
